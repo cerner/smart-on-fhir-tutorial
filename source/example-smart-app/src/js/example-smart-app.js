0e1fae6fc690e8a05597fd567c557d18935c3079
Kheang, Kol <kol.kheang@cerner.com>
2016-11-17 19:20:39 -0600

    Fix NPE when valueQuantity is undefined

--- source/example-smart-app/src/js/example-smart-app.js
+++ source/example-smart-app/src/js/example-smart-app.js
@@ -12,15 +12,15 @@
         var patient = smart.patient;
         var pt = patient.read();
         var obv = smart.patient.api.fetchAll({
-                      type: 'Observation',
-                      query: {
-                        code: {
-                          $or: ['http://loinc.org|8302-2', 'http://loinc.org|8462-4',
-                                'http://loinc.org|8480-6', 'http://loinc.org|2085-9',
-                                'http://loinc.org|2089-1', 'http://loinc.org|55284-4']
-                              }
-                             }
-                    });
+                    type: 'Observation',
+                    query: {
+                      code: {
+                        $or: ['http://loinc.org|8302-2', 'http://loinc.org|8462-4',
+                              'http://loinc.org|8480-6', 'http://loinc.org|2085-9',
+                              'http://loinc.org|2089-1', 'http://loinc.org|55284-4']
+                      }
+                    }
+                  });
 
         $.when(pt, obv).fail(onError);
 
@@ -36,7 +36,7 @@
           var fname = '';
           var lname = '';
 
-          if(typeof patient.name[0] !== 'undefined') {
+          if (typeof patient.name[0] !== 'undefined') {
             fname = patient.name[0].given.join(' ');
             lname = patient.name[0].family.join(' ');
           }
@@ -53,26 +53,19 @@
           p.fname = fname;
           p.lname = lname;
           p.age = parseInt(calculateAge(dob));
+          p.height = getQuantityValueAndUnit(height[0]);
 
-          if(typeof height[0] != 'undefined' && typeof height[0].valueQuantity.value != 'undefined' && typeof height[0].valueQuantity.unit != 'undefined') {
-            p.height = height[0].valueQuantity.value + ' ' + height[0].valueQuantity.unit;
-          }
-
-          if(typeof systolicbp != 'undefined')  {
+          if (typeof systolicbp != 'undefined')  {
             p.systolicbp = systolicbp;
           }
 
-          if(typeof diastolicbp != 'undefined') {
+          if (typeof diastolicbp != 'undefined') {
             p.diastolicbp = diastolicbp;
           }
 
-          if(typeof hdl[0] != 'undefined' && typeof hdl[0].valueQuantity.value != 'undefined' && typeof hdl[0].valueQuantity.unit != 'undefined') {
-            p.hdl = hdl[0].valueQuantity.value + ' ' + hdl[0].valueQuantity.unit;
-          }
+          p.hdl = getQuantityValueAndUnit(hdl[0]);
+          p.ldl = getQuantityValueAndUnit(ldl[0]);
 
-          if(typeof ldl[0] != 'undefined' && typeof ldl[0].valueQuantity.value != 'undefined' && typeof ldl[0].valueQuantity.unit != 'undefined') {
-            p.ldl = ldl[0].valueQuantity.value + ' ' + ldl[0].valueQuantity.unit;
-          }
           ret.resolve(p);
         });
       } else {
@@ -100,7 +93,7 @@
     };
   }
 
-  function getBloodPressureValue(BPObservations, typeOfPressure){
+  function getBloodPressureValue(BPObservations, typeOfPressure) {
     var formattedBPObservations = [];
     BPObservations.forEach(function(observation){
       var BP = observation.component.find(function(component){
@@ -114,12 +107,7 @@
       }
     });
 
-    if (typeof formattedBPObservations[0] != 'undefined' && typeof formattedBPObservations[0].valueQuantity.value != 'undefined' && formattedBPObservations[0].valueQuantity.unit != 'undefined') {
-      return formattedBPObservations[0].valueQuantity.value + ' ' + formattedBPObservations[0].valueQuantity.unit ;
-    }
-    else {
-      return undefined;
-    }
+    return getQuantityValueAndUnit(formattedBPObservations[0]);
   }
 
   function isLeapYear(year) {
@@ -141,7 +129,17 @@
     else {
       return undefined;
     }
+  }
 
+  function getQuantityValueAndUnit(ob) {
+    if (typeof ob != 'undefined' &&
+        typeof ob.valueQuantity != 'undefined' &&
+        typeof ob.valueQuantity.value != 'undefined' &&
+        typeof ob.valueQuantity.unit != 'undefined') {
+          return ob.valueQuantity.value + ' ' + ob.valueQuantity.unit;
+    } else {
+      return undefined;
+    }
   }
 
   window.drawVisualization = function(p) {
