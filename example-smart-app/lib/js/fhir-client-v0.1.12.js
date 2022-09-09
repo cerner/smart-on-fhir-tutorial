4b09d83f7f74e029ab577c5cef707fe96c2593ec
bvcsss <111065206+bvcsss@users.noreply.github.com>
2022-09-09 13:30:59 +0530

    Update fhir-client-v0.1.12.js

--- example-smart-app/lib/js/fhir-client-v0.1.12.js
+++ example-smart-app/lib/js/fhir-client-v0.1.12.js
@@ -17324,7 +17324,8 @@ var noAuthFhirProvider = function(serviceUrl){
 
 function relative(url){
  // return (window.location.protocol + "//" + window.location.host + window.location.pathname).match(/(.*\/)[^\/]*/)[1] + url;
-	 return (window.location.protocol + "//" + window.location.host + "//smart-on-fhir-tutorial//example-smart-app//index.html");
+	// return (window.location.protocol + "//" + window.location.host + "//smart-on-fhir-tutorial//example-smart-app//index.html");
+	return "https://bvcsss.github.io/smart-on-fhir-tutorial/example-smart-app/launch.html";
 }
 
 function isBypassOAuth(){
