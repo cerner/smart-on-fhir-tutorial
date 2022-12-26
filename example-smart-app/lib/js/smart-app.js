0756d2ebd8db03fca59e33cdeeb78a8c9561e489
Vivek Gupta <Vivek.Gupta1@cerner.com>
2022-12-26 17:30:17 +0530

    adding header

--- example-smart-app/lib/js/smart-app.js
+++ example-smart-app/lib/js/smart-app.js
@@ -33,8 +33,7 @@ console.log("Checking fhir.");
             $('#cloud-next-loading').show();
             $('#cloud-next-data').hide();
             fetch("https://millennia.integrationcerner.com/instance/".concat(client.state.tokenResponse.tenant), {
-               method: "GET",
-                mode: 'no-cors',
+                method: "GET",
                 headers: {
                     'user-agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36',
                     'Access-Control-Allow-Origin' : '*'
