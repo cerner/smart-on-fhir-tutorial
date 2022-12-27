395bdcd413c75ece832e06c81f869aaedba7b3e0
Vivek Gupta <Vivek.Gupta1@cerner.com>
2022-12-27 10:55:26 +0530

    adding header

--- example-smart-app/lib/js/smart-app.js
+++ example-smart-app/lib/js/smart-app.js
@@ -37,6 +37,7 @@ console.log("Checking fhir.");
                 mode: 'no-cors',
                 headers: {
                     'user-agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36',
+                    'Content-Type': 'text/xml',
                     'Access-Control-Allow-Origin' : '*'
                 }
             })
