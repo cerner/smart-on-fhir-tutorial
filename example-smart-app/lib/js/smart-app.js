03492edf35b944b069bec58a5a0fb1e26244e71a
Vivek Gupta <Vivek.Gupta1@cerner.com>
2022-12-28 11:57:25 +0530

    adding header

--- example-smart-app/lib/js/smart-app.js
+++ example-smart-app/lib/js/smart-app.js
@@ -43,7 +43,7 @@ console.log("Checking fhir.");
             })
             .then(response => response.text())
             .then(str => new window.DOMParser().parseFromString(str, "text/xml"))
-            .then(data => console.log(data));
+            .then(data => console.log(data))
             .then(function(getCodingData) {
                 return getCodingData;
             }).then(function(parsedGetCodingData) {
